379683a5f6fcf9d6f9cf555bc85ac17f3e0e606a
CharelM <3103120057@student.smktelkom-pwt.sch.id>
2022-11-30 09:01:59 +0700

    UAS

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KamarController;
+use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Kamar
+    Route::apiResource('kamars', KamarController::class);
+    
+    //transaksi
+    Route::apiResource('transaksi', TransaksiController::class);
+
+    //Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
